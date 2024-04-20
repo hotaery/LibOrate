918765c7f43a02321de54bef9b44cc07c1b4ef8a
Shaomei <shaomei@gmail.com>
2024-04-19 17:35:17 -0700

    Refactor the mindfulness page (#42)
    1. Put video related information into data/videos.json to separate data and
       code;
    2. Use array.map to loop over existing videos and reduce code redudancy;
    3. Update video aria-label with alt-text that visually describes the video
       to screen readers;
    4. Add 'key' attribute to video player component as recommended by React
       warning messages;
    5. Update and simplify related unit tests.

--- __tests__/mindfulness.test.tsx
+++ __tests__/mindfulness.test.tsx
@@ -1,42 +1,28 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from '../app/main/Mindfulness'
-
+import videoList from '@/data/videos.json'; // Import video data
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 jest.mock('../lib/zoomapi', () => jest.requireActual('../lib/fakezoomapi'));
 
 describe('Page', () => {
-  it('renders a heading and three videos', () => {
+  it('renders a heading and available meditation videos', () => {
     render(<Page />)
 
-
-    const video1 = screen.getByLabelText('youtube-video-1');
-    expect(video1).toBeInTheDocument();
-    
-    const video2 = screen.getByLabelText('youtube-video-2');
-    expect(video2).toBeInTheDocument();
-
-    const video3 = screen.getByLabelText('youtube-video-3');
-    expect(video3).toBeInTheDocument();
+    videoList.map((video, index) => {
+        expect(screen.getByLabelText(video.alt)).toBeInTheDocument();
+    });
   })
 
   it('renders three correct videos', () => {
     render(<Page />)
 
-    const youtube1ID = 'AKN_gbj8UtU'
-    const youtube2ID = 'c7IkEhKtUwI'
-    const youtube3ID = 'sG69_omRCyo'
-
-    const video1 = screen.getByLabelText('youtube-video-1');
-    expect(video1).toHaveAttribute('src', expect.stringContaining(youtube1ID));
-
-    const video2 = screen.getByLabelText('youtube-video-2');
-    expect(video2).toHaveAttribute('src', expect.stringContaining(youtube2ID));
-
-    const video3 = screen.getByLabelText('youtube-video-3');
-    expect(video3).toHaveAttribute('src', expect.stringContaining(youtube3ID));
-
+    videoList.map((video, index) => {
+    expect(screen.getByLabelText(video.alt)).toHaveAttribute(
+        'src',
+        expect.stringContaining(video.youtubeId)
+    );
+    });
   })
-
 })
--- app/main/Mindfulness.tsx
+++ app/main/Mindfulness.tsx
@@ -1,41 +1,41 @@
-// Under Review
-
 "use client";
 
-import '../css/Mindfulness.css'; // Import CSS file
+import '../css/Mindfulness.css';
+import videos from '@/data/videos.json'; // Import video meta data
 
 interface VideoPlayerProps {
+  name: string;
   videoId: string;
-  Label: string;
+  label: string;
+  alt: string;
 }
 
-const generateVideoPlayer = ({ videoId, Label }: VideoPlayerProps) => (
-  <div className="video-wrapper">
+const generateVideoPlayer = (
+    { name, videoId, label, alt }: VideoPlayerProps
+    ) => (
+  <div className="video-wrapper" key={label}>
     <iframe
       className='video'
-      title='Youtube player'
-      aria-label={Label}
+      title={name}
+      aria-label={alt}
       src={`https://www.youtube.com/embed/${videoId}`}
     ></iframe>
   </div>
 );
 
 function Mindfullness() {
-
-  const youtube1ID = 'AKN_gbj8UtU'
-  const youtube2ID = 'c7IkEhKtUwI'
-  const youtube3ID = 'sG69_omRCyo'
-  const youtube1Label = 'youtube-video-1';
-  const youtube2Label = 'youtube-video-2';
-  const youtube3Label = 'youtube-video-3';
-
   return (
     <div className="mindfulness-container">
       <h2 className="title">Mindfulness</h2>
       <div className="video-container">
-        {generateVideoPlayer({ videoId: youtube1ID, Label: youtube1Label })}
-        {generateVideoPlayer({ videoId: youtube2ID, Label: youtube2Label })}
-        {generateVideoPlayer({ videoId: youtube3ID, Label: youtube3Label })}
+        {videos.map((video, index) => 
+            generateVideoPlayer({
+                name: video.name,
+                videoId: video.youtubeId,
+                label: video.label,
+                alt: video.alt
+            })
+        )}
       </div>
     </div>
   );
