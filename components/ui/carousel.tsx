48052a5e7f97a22695064203774b722efe53b237
Sonia Sunil <37371431+ssunil3232@users.noreply.github.com>
2024-06-30 10:33:33 -0400

    fix to chevron disable on prev & next button nav (#83)
    Co-authored-by: “sunil3232” <“soniasunil47@gmail.com”>

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -206,12 +206,14 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute  h-8 w-8 rounded-full flex items-center justify-center",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
-        className
+        className,
+        canScrollPrev ? 'carousel-button': 'carousel-button-disabled'
       )}
+      style={{ paddingLeft: '0px!important', paddingRight: '0px!important', width: '2.6rem!important' }}
       disabled={!canScrollPrev}
       onClick={scrollPrev}
       {...props}
@@ -235,12 +237,14 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "absolute h-8 w-8 rounded-full flex items-center justify-center",
         orientation === "horizontal"
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
-        className
+        className,
+        canScrollNext ? 'carousel-button': 'carousel-button-disabled'
       )}
+      style={{ paddingLeft: '0px!important', paddingRight: '0px!important', width: '2.6rem!important'}}
       disabled={!canScrollNext}
       onClick={scrollNext}
       {...props}
